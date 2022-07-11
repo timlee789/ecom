b2a35497d3b9c5eda49d43df7f25a7c7ca63abe2
timlee789 <systeco@hotmail.com>
2022-07-11 17:14:20 -0400

    0711-5

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -2,9 +2,12 @@ import React, { useState } from 'react'
 import { client, urlFor } from '../../lib/client';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar} from 'react-icons/ai';
 import { Product } from '../../components';
+import { useStateContext} from '../../context/StateContext';
+
 function ProductDetails({product, products}) {
         const { image, name, details, price } = product;
         const [index, setIndex] = useState(0)
+        const { decQty, incQty, qty, onAdd } = useStateContext();
   return (
     <div>
         <div className='product-detail-container'>
@@ -39,19 +42,19 @@ function ProductDetails({product, products}) {
                                 <div className='quantity'>
                                         <h3>Quantity:</h3>
                                         <p className="quantity-desc">
-                                                <span className='minus' onClick=''>
+                                                <span className='minus' onClick={decQty}>
                                                         <AiOutlineMinus />
                                                 </span>
                                                 <span className='num' onClick=''>
-                                                        0
+                                                        {qty}
                                                 </span>
-                                                <span className='plus' onClick=''>
+                                                <span className='plus' onClick={incQty}>
                                                         <AiOutlinePlus />
                                                 </span>
                                         </p>
                                 </div>
                                 <div className='buttons'>
-                                        <button type='button' className='add-to-cart' onClick=''>
+                                        <button type='button' className='add-to-cart' onClick={() => onAdd(product,qty)}>
                                                 Add to Cart
                                         </button>
                                         <button type='button' className='buy-now' onClick=''>
