99550c2b254b47e8b41227e7561184e01a63fa35
timlee789 <84083360+timlee789@users.noreply.github.com>
2022-07-10 21:29:54 -0400

    0710-1

---
+++ components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { AiFIllInstagram, AiOutlineTwistter} from 'react-icons/ai';
+function Footer() {
+  return (
+    <div className='footer-container'>
+      <p>2022 Beauty Elements All rights reserved</p>
+      <p className='icons'>
+        
+      </p>
+
+    </div>
+  )
+}
+
+export default Footer
